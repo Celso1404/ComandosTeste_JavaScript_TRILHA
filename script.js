5e7a1a52a6895e60a84ec61113e03dfb60da3ad8
Celso Bitello <celsokaiiki@gmail.com>
2026-04-26 23:49:59 -0300

    Adicionando testes

---
+++ script.js
@@ -0,0 +1,71 @@
+let a = 10;
+const b = 20;
+var c = 30;
+
+var cor = "Amarelo";
+
+//Condições
+//if (cor === "verde") {
+//    console.log("Siga");
+//} else if (cor === "amarelo"){ 
+   // console.log("Siga com cuidado")
+//} else {
+  //  console.log("Pare");
+//}
+
+//switch (cor.toLowerCase()) {
+
+ //   case "verde": 
+ //   console.log("siga");
+ //   break;
+
+ //   case "amarelo":
+   //     console.log("Cuidado");
+   //     break;
+   
+  //  case "vermelho":
+    //    console.log("Pare!");
+    //    break;
+   // default:
+   //     console.log("Não sei");
+    //    break;
+
+//}
+
+//Tabuada
+//let n = 10;
+
+//for (let i = 0; i <= 10; i++) {
+
+  //  console.log(`${i} X ${n} = ${i*n}`);
+
+//}
+
+//function somar(x1, x2, operator) {
+
+ //   return eval(`${x1} ${operator} ${x2}`);
+//}
+
+//let resultado = somar(10, 20, "*")
+//console.log(`Resultado = ${resultado}`)
+
+
+//Arrow Function
+//let calc = (x1, x2, operator) => {
+//    return `${x1} ${operator} ${x2}`
+//}
+
+
+//ADICIONANDO EVENTOS
+//.addEventListener('focus', event => {
+
+  //  console.log("Focus")
+
+//})
+
+//document.addEventListener('click', event => {
+   // console.log("click")
+//})
+
+//let agora = new Date()
+//console.log(agora.toLocaleDateString("pt-BR"))
